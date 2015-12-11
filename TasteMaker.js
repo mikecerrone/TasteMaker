42f0d4986585163583c0d70536b4c0b2a7f6e1a4
r.sinclairchin <r.sinclairchin@gmail.com>
2015-12-11 12:13:40 -0800

    add buttons for taste profile, wine history and reccommendations, events for each to hide/show divs containing templates for each, home button with sweet homing action

--- TasteMaker.js
+++ TasteMaker.js
@@ -3,21 +3,47 @@ if (Meteor.isClient) {
   Session.setDefault('counter', 0);
 
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+    // counter: function () {
+    //   return Session.get('counter');
+    // }
   });
 
   Template.hello.events({
-    'click button': function () {
+    'click #taste': function () {
       // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      // Session.set('counter', Session.get('counter') + 1);
+      document.querySelector("#tasteDiv").classList.toggle("hide")
+      document.querySelector("#helloDiv").classList.toggle("hide")
+    },
+    'click #history': function () {
+      // increment the counter when button is clicked
+      // Session.set('counter', Session.get('counter') + 1);
+      document.querySelector("#historyDiv").classList.toggle("hide")
+      document.querySelector("#helloDiv").classList.toggle("hide")
+    },
+    'click #rec': function () {
+      // increment the counter when button is clicked
+      // Session.set('counter', Session.get('counter') + 1);
+      document.querySelector("#recDiv").classList.toggle("hide")
+      document.querySelector("#helloDiv").classList.toggle("hide")
     }
   });
-}
+
+  Template.home.events({
+    'click #home': function(){
+      document.querySelector("#recDiv").classList.add("hide")
+      document.querySelector("#historyDiv").classList.add("hide")
+      document.querySelector("#tasteDiv").classList.add("hide")
+      document.querySelector("#helloDiv").classList.toggle("hide")
+    }
+//     'click #home': function(){
+//       document.querySelector(this.parent).classList.toggle("hide")
+//     }
+  })
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}
 }

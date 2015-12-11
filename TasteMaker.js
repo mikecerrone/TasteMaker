a43d122d94e24e9f2be32882192ffdf2ca5f438b
r.sinclairchin <r.sinclairchin@gmail.com>
2015-12-11 14:42:43 -0800

    add method for ajax call to wine.com

--- TasteMaker.js
+++ TasteMaker.js
@@ -1,11 +1,31 @@
+Wines = new Mongo.Collection("wines");
+
 if (Meteor.isClient) {
+
+  // Meteor.publish("wines", function(){
+  //   return Wines.find({
+
+  //   })
+  // })
+
+  Meteor.call("getWines", function(error, results){
+    console.log(results.content);
+  })
+
   // counter starts at 0
   Session.setDefault('counter', 0);
 
+  Template.home.events({
+    'click #home': function(){
+      document.querySelector("#recDiv").classList.add("hide")
+      document.querySelector("#historyDiv").classList.add("hide")
+      document.querySelector("#tasteDiv").classList.add("hide")
+      document.querySelector("#helloDiv").classList.toggle("hide")
+    }
+  })
+
   Template.hello.helpers({
-    // counter: function () {
-    //   return Session.get('counter');
-    // }
+
   });
 
   Template.hello.events({
@@ -29,21 +49,18 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.home.events({
-    'click #home': function(){
-      document.querySelector("#recDiv").classList.add("hide")
-      document.querySelector("#historyDiv").classList.add("hide")
-      document.querySelector("#tasteDiv").classList.add("hide")
-      document.querySelector("#helloDiv").classList.toggle("hide")
-    }
-//     'click #home': function(){
-//       document.querySelector(this.parent).classList.toggle("hide")
-//     }
-  })
+}
+
+
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    Meteor.methods({
+      getWines: function () {
+          return Meteor.http.call("GET", "https://services.wine.com/api/beta2/service.svc/json/catalog?search=apothic%20red&size=1&apikey=f6569a177b45d11f2e5dc5fee4bf9e82");
+      }
+    });
   });
-}
 }

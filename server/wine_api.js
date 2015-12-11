09bb932c0a666fc2b7c88a3c9ca1207badac98b4
Shawn Watson <shawnw86@gmail.com>
2015-12-11 15:39:46 -0800

    Add ajaxWineApi method with hard coded URL

--- server/wine_api.js
+++ server/wine_api.js
@@ -1,3 +1,7 @@
 Meteor.methods({
-    
+    ajaxWineApi: function() {
+    // MUST BE UPDATED WITH ALGORITHM
+        this.unblock;
+        return Meteor.http.call("GET", 'https://services.wine.com/api/beta2/service.svc/json/catalog?search=apothic%20red&size=1&apikey=f6569a177b45d11f2e5dc5fee4bf9e82')
+    }
 })

cb4bc78697a62de6112e2b833cf315fd64ee1ea7
Shawn Watson <shawnw86@gmail.com>
2015-12-11 17:24:54 -0800

    Add login to page, and add fake search

--- TasteMaker.js
+++ TasteMaker.js
@@ -7,16 +7,9 @@ if (Meteor.isClient) {
 
   //   })
   // })
-
-  Meteor.call("getWines", function(error, results){
-    console.log(results.content);
-  })
-
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.home.events({
-    'click #home': function(){
+  
+  Meteor.methods({
+    hideAll: function() {
       document.querySelector("#recDiv").classList.add("hide")
       document.querySelector("#historyDiv").classList.add("hide")
       document.querySelector("#tasteDiv").classList.add("hide")
@@ -24,6 +17,20 @@ if (Meteor.isClient) {
     }
   })
 
+  // Meteor.call("getWines", function(error, results){
+  //   console.log(results.content);
+  // })
+
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.home.events({
+    'click #home': function(){
+      Meteor.call('hideAll');
+    }
+  })
+
+    
   Template.hello.helpers({
 
   });
@@ -49,18 +56,30 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.recTemplate.events({
+    'click #getRec': function() {
+      Meteor.call("wineApiRecommendation", function(err, res) {
+        // console.log(test)
+        var results = res.data.Products.List[0]
+        // debugger;
+        // console.log(res.data.Products.List[0])
+        Blaze.renderWithData(Template.recReturned, {results: results}, document.querySelector('#recResult'))
+      })
+    }
+  })
+
 }
 
 
 
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
+// if (Meteor.isServer) {
+//   Meteor.startup(function () {
     // code to run on server at startup
-    Meteor.methods({
-      getWines: function () {
-          return Meteor.http.call("GET", "https://services.wine.com/api/beta2/service.svc/json/catalog?search=apothic%20red&size=1&apikey=f6569a177b45d11f2e5dc5fee4bf9e82");
-      }
-    });
-  });
-}
+    // Meteor.methods({
+    //   getWines: function () {
+    //       return Meteor.http.call("GET", "https://services.wine.com/api/beta2/service.svc/json/catalog?search=apothic%20red&size=1&apikey=f6569a177b45d11f2e5dc5fee4bf9e82");
+    //   }
+//     });
+//   });
+// }

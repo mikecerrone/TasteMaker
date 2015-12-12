41f5e25f9c563d251842edad44c43dbe83509a55
PaulEtscheit <etscheitp@gmail.com>
2015-12-12 13:24:08 -0800

    scanner not coupled with button yet, investigation slider issue

--- upc_scanner.js
+++ upc_scanner.js
@@ -1,15 +1,19 @@
 Meteor.startup(function () {
-  if (Meteor.isCordova){
-    cordova.plugins.barcodeScanner.scan(
-      function (result) {
+  Template.body.events ({
+    'click #wineRec': function(event) {
+      if (Meteor.isCordova){
+        cordova.plugins.barcodeScanner.scan(
+        function (result) {
         alert(result.text)
         Meteor.call('upcDecoder', result, function(error, results) {
-        alert(results[0].product_name)
-      });
-      },
-      function (error) {
+          alert(results[0].product_name)
+        });
+        },
+        function (error) {
           alert("Scanning failed: " + error);
-      }
-    );
-  }
+        }
+
+      )
+    }
+  }})
 })

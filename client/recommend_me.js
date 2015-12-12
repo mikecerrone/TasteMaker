570c2c978691d53d9d88da7037d67f0857fe2145
Shawn Watson <shawnw86@gmail.com>
2015-12-12 09:11:51 -0800

    Render wine recommendation template

---
+++ client/recommend_me.js
@@ -0,0 +1,17 @@
+Template.body.events ({
+    'click #wineRec': function(event) {
+        event.preventDefault(); 
+        $('#pageHome').toggleClass('hide');
+        $('#pageDisplay').toggleClass('hide')
+        // Blaze.renderWithData(Template.recReturned, {results: results}, document.querySelector('#recResult'))
+        Blaze.render(Template.recWine, document.querySelector('#pageDisplay'))
+    }
+});
+
+Template.recWine.events ({
+    'click button': function(event) {
+        event.preventDefault();
+        $('#pageHome').toggleClass('hide');
+        $('#pageDisplay').toggleClass('hide')
+    }
+})

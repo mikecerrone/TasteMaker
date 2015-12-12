fc2881431259ca32d73c889b747b7e7d2f15bfd1
Shawn Watson <shawnw86@gmail.com>
2015-12-12 10:47:56 -0800

    Added voting and taste page

---
+++ client/wine_rating.js
@@ -0,0 +1,18 @@
+Template.body.events ({
+    'click #scanIt': function(event) {
+        event.preventDefault(); 
+        $('#pageHome').toggleClass('hide');
+        $('#pageDisplay').toggleClass('hide')
+        // Blaze.renderWithData(Template.recReturned, {results: results}, document.querySelector('#recResult'))
+        render = Blaze.render(Template.rateWine, document.querySelector('#pageDisplay'))
+    }
+});
+
+Template.rateWine.events ({
+    'click button': function(event) {
+        event.preventDefault();
+        $('#pageHome').toggleClass('hide');
+        $('#pageDisplay').toggleClass('hide')
+        Blaze.remove(render)
+    }
+})

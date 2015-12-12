339a9f7230f863cfdbe3bd3bf88c6d433c40de9f
Shawn Watson <shawnw86@gmail.com>
2015-12-11 16:08:44 -0800

    Update wine API for viewing a wine, add placeholder method for recommendation'

--- server/wine_api.js
+++ server/wine_api.js
@@ -1,7 +1,12 @@
 Meteor.methods({
-    ajaxWineApi: function() {
-    // MUST BE UPDATED WITH ALGORITHM
-        this.unblock;
-        return Meteor.http.call("GET", 'https://services.wine.com/api/beta2/service.svc/json/catalog?search=apothic%20red&size=1&apikey=f6569a177b45d11f2e5dc5fee4bf9e82')
+    wineApiLookup: function(name) { 
+        var name = name
+        this.unblock();
+        return Meteor.http.call("GET", 'https://services.wine.com/api/beta2/service.svc/json/catalog?search=' + name + '&apikey=f6569a177b45d11f2e5dc5fee4bf9e82')
+    }
+
+    wineApiRecommendation: function() {
+        this.unblock()
+        // call our algorithm here
     }
 })

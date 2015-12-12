0afc3a0f79d896d71d4544686a0e2eb115d9874d
r.sinclairchin <r.sinclairchin@gmail.com>
2015-12-11 20:47:23 -0800

    add real wines from a real apilookup method into the database with user_id and everything

--- server/wine_api.js
+++ server/wine_api.js
@@ -1,5 +1,5 @@
 Meteor.methods({
-    wineApiLookup: function(name) { 
+wineApiLookup: function(name) {
         var name = name
         this.unblock();
         return Meteor.http.call("GET", 'https://services.wine.com/api/beta2/service.svc/json/catalog?search=' + name + '&apikey=f6569a177b45d11f2e5dc5fee4bf9e82')

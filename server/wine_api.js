ccdf87748b536a458bdacff314f0dc8f1d63077c
PaulEtscheit <etscheitp@gmail.com>
2015-12-12 21:35:03 -0800

    very dirty code, attempting smart selections but moving on for now

--- server/wine_api.js
+++ server/wine_api.js
@@ -12,8 +12,43 @@ Meteor.methods({
   },
 
   wineApiLookupSorting: function(results) {
-    console.log('hit next')
-    console.log(results.content)
+    var wines = {}
+    var wineNoYears = {}
+    var parsedResponse = JSON.parse(results.content);
+    var responseArray = parsedResponse.Products.List
+    console.log(responseArray.length)
+    for (i = 0; i < responseArray.length; i++) {
+      var nameSplit = responseArray[i].Name.split(" ");
+      var year = nameSplit.pop();
+      var name = nameSplit.join(" ")
+      var region = responseArray[i].Appellation.Region.Name
+      // if (responseArray[i].ProductAttributes.length > 0) {
+      //   var style = responseArray[i].ProductAttributes[0].Name
+      // }
+      var varietal = responseArray[i].Varietal.Name
+      var type = responseArray[i].Varietal.WineType.Name
+      var price = responseArray[i].PriceMax
+      if(responseArray[i].Ratings.HighestScore > 0) {
+        var rating = responseArray[i].Ratings.HighestScore
+      } else {
+        var rating = null
+      }
+      console.log(i)
+      wines[responseArray[i].Name] = {year: year, price: price, region: region, varietal: varietal, type: type, rating: rating}
+      wineNoYears.push(name)
+     var wineNoYearsDone = $.unique(wineNoYears);
+
+    }
+
+    Meteor.call('resultLogic', wineNoYearsDone, wines, responseArray.length)
+  },
+
+  resultLogic: function(noYears, allWines, wineCount) {
+    console.log(wineCount)
+    console.log("here")
+    console.log(noYears.length)
+    console.log((wineCount - noYears.length) > wineCount * 0.6)
+    // if ((wineCount - noYears.length) > wineCount * 0.6)
   },
 
   wineApiRecommendation: function() {

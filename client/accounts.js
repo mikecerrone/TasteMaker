c9cdb70695333455a4a188042c680098dadeb62b
Shawn Watson <shawnw86@gmail.com>
2015-12-13 11:53:14 -0800

    Add user accounts login and registration

--- client/accounts.js
+++ client/accounts.js
@@ -9,3 +9,24 @@ Template.accountBox.events ({
         $('.logIn').removeClass('hide');   
     }
 })
+
+Template.signUp.events ({
+    'submit form': function(event) {
+        event.preventDefault();
+        var emailVar = event.target.signupEmail.value;
+        var passwordVar = event.target.signupPassword.value;
+        Accounts.createUser({
+            email: emailVar,
+            password: passwordVar
+        });
+    }
+});
+
+Template.logIn.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var emailVar = event.target.loginEmail.value;
+        var passwordVar = event.target.loginPassword.value;
+        Meteor.loginWithPassword(emailVar, passwordVar)
+    }
+});

eea7218100d55cc55f8db31e01a16f3799de1b83
PaulEtscheit <etscheitp@gmail.com>
2015-12-12 17:07:14 -0800

    working on response sorting

--- server/wine_api.js
+++ server/wine_api.js
@@ -1,12 +1,23 @@
 Meteor.methods({
-wineApiLookup: function(name) {
-        var name = name
-        this.unblock();
-        return Meteor.http.call("GET", 'https://services.wine.com/api/beta2/service.svc/json/catalog?search=' + name + '&apikey=f6569a177b45d11f2e5dc5fee4bf9e82')
-    },
+  wineApiLookup: function(name) {
+    var uriName = encodeURI(name);
+    console.log('hit api request method');
+    this.unblock();
+    console.log("before")
+    var results = Meteor.http.call("GET", 'https://services.wine.com/api/beta2/service.svc/json/catalog?search=' + uriName + '&apikey=f6569a177b45d11f2e5dc5fee4bf9e82');
 
-    wineApiRecommendation: function() {
-        this.unblock()
-        return Meteor.http.call("GET", 'https://services.wine.com/api/beta2/service.svc/json/catalog?search=apothic%20red&apikey=f6569a177b45d11f2e5dc5fee4bf9e82')
-    }
+    console.log(results);
+    console.log("after");
+    Meteor.call('wineApiLookupSorting', results);
+  },
+
+  wineApiLookupSorting: function(results) {
+    console.log('hit next')
+    console.log(results.content)
+  },
+
+  wineApiRecommendation: function() {
+    this.unblock()
+    return Meteor.http.call("GET", 'https://services.wine.com/api/beta2/service.svc/json/catalog?search=apothic%20red&apikey=f6569a177b45d11f2e5dc5fee4bf9e82')
+  }
 })
--- server/wine_lookup.js
+++ server/wine_lookup.js
@@ -8,8 +8,16 @@ Meteor.methods({
   upcDecoder: function (upcCode) {
     Factual.get('/t/products-cpg',{q:upcCode.text},
       function (error, res) {
-        console.log("hit me")
-         console.log(res);
+        console.log(res);
+        console.log(error);
+        if (res.data.length > 0){
+          var wineName = res.data[0].brand
+          Meteor.call('wineApiLookup', wineName, function(err, res){
+          })
+        } else {
+          //render manual search page...
+          console.log("saved")
+        }
       });
   }
 });

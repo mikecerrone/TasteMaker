4bc1eeac2078bafabea1d02db77ff91bd3822f17
Shawn Watson <shawnw86@gmail.com>
2015-12-13 09:18:15 -0800

    Add color to manual search page

--- client/about_us.js
+++ client/about_us.js
@@ -15,7 +15,6 @@ Template.notFound.events({
             var wineName = res.data.Products.List[0].Name
             var wineUrl =  res.data.Products.List[0].Labels[0].Url
             var wineType =  res.data.Products.List[0].ProductAttributes[0].Name
-            // debugger;
             Blaze.remove(render)
             render = Blaze.renderWithData(Template.rateWine, {name: wineName, url: wineUrl, type: wineType}, document.querySelector('#pageDisplay'))
         })

ec98a0dd7830db51fc67f14cb8d27be46a3c97b2
r.sinclairchin <r.sinclairchin@gmail.com>
2015-12-13 16:51:46 -0800

    move the collection OUTSIDE OF THE CLIENTFOLDER

--- client/wine_history.js
+++ client/wine_history.js
@@ -1,3 +1,5 @@
+// UserHistory = new Mongo.Collection("history");
+
 Template.body.events ({
     'click #wineHistory': function(event) {
         event.preventDefault();
@@ -7,6 +9,11 @@ Template.body.events ({
         $('#pageHome').addClass('hide');
         $('#pageDisplay').removeClass('hide')
         render = Blaze.render(Template.wineHistory, document.querySelector('#pageDisplay'))
+    },
+
+    'scan': function() {
+        historyInsert();
+
     }
 })
 
@@ -19,3 +26,48 @@ Template.wineHistory.events ({
     }
 })
 
+if (Meteor.isClient){
+
+    var scanWine = {name: 'Apothic Red Blend 2012',
+                    year: '2012',
+                    price: 11.99,
+                    style: 'Smooth &amp; Supple',
+                    region: 'California',
+                    varietal: 'Other Red Blends',
+                    type: 'Red Wines',
+                    rating: 88
+    };
+
+    var hey = {hello: "hi"}
+
+    Template.wineHistory.rendered = function(){
+        console.log("rendered");
+        historyInsert(scanWine);
+        // Meteor.call("historyInsert", scanWine);
+    };
+
+};
+
+historyInsert = function(scanWine){
+    console.log(scanWine)
+    UserHistory.insert({
+        name: scanWine.name
+    })
+    console.log(UserHistory.find({}))
+}
+
+// Meteor.methods({
+
+//     historyInsert: function(scanWine){
+
+//         console.log(scanWine)
+
+//         var name = scanWine.name
+
+//         // UserHistory.insert({
+//         //     name: name
+//         // })
+
+//     }
+
+// });

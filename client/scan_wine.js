76e7083d9b30ffb4cebf210f7b83a99522766481
PaulEtscheit <etscheitp@gmail.com>
2015-12-15 14:45:47 -0800

    search result array complete, sorted by match %

--- client/scan_wine.js
+++ client/scan_wine.js
@@ -2,13 +2,15 @@ Template.body.events ({
     "click #scanIt": function(event) {
         event.preventDefault();
         if (Meteor.isCordova) {
-            Meteor.call('barcodeScan', function(err, res) {
+              $('#pageHome').addClass('hide');
+              $('#pageDisplay').removeClass('hide');
+              Meteor.call('barcodeScan', function(err, res) {
                 // render = Blaze.render(Template.rateWine, document.querySelector('#pageDisplay'))
             })
         } else {
-            $('#pageHome').toggleClass('hide');
-            $('#pageDisplay').toggleClass('hide');
-            render = Blaze.render(Template.notFound, document.querySelector('#pageDisplay'))
+              $('#pageHome').addClass('hide');
+              $('#pageDisplay').removeClass('hide');
+              render = Blaze.render(Template.notFound, document.querySelector('#pageDisplay'))
         }
     }
 })

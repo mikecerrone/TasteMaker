12a0785757becf963df5430737d125d679f3a982
PaulEtscheit <etscheitp@gmail.com>
2015-12-16 09:58:05 -0800

    pre reboot commit

--- client/recommend_me.js
+++ client/recommend_me.js
@@ -54,7 +54,6 @@ Template.body.events ({
         event.preventDefault();
         $('#pageHome').addClass('hide');
         $('#pageDisplay').removeClass('hide')
-        // Blaze.renderWithData(Template.recReturned, {results: results}, document.querySelector('#recResult'))
         render = Blaze.render(Template.recWine, document.querySelector('#pageDisplay'))
     }
 });
@@ -68,37 +67,14 @@ Template.recWine.events ({
         Blaze.remove(render)
     }
 })
-
-
-
-
-// find closest number in an array
-
-
-// function tasteStager(){
-//     // gets each tasteProfile array and adds into and array
-//     // Meteor.subscribe("Taste", function(){
-//       var splitUpTheTasteArrays = []
-//       // UserTasteProfile being created here: )does not currently work
-//      var tastes =  Taste.find({user: Meteor.userId()}).forEach(function(taste){splitUpTheTasteArrays.push(taste.userTaste)})
-//         console.log(splitUpTheTasteArrays)
-//         console.log(something);
-//         return (splitUpTheTasteArrays)
-//     // })
-//     }
-Template.body.rendered = function() {
-var splitUpTheTasteArrays1 = []
-    var tastes = Taste.find({})
-    tastes.forEach(function(tastes){splitUpTheTasteArrays1.push(tastes.userTaste)})
-    // .forEach(function(taste){splitUpTheTasteArrays1.push(taste.userTaste)})
-       console.log(splitUpTheTasteArrays1)
-       console.log(something);
-
-   tasteAverage(splitUpTheTasteArrays1)
+spiltTastes = function() {
+  var splitUpTheTasteArrays1 = []
+  var tastes = Taste.find({})
+  tastes.forEach(function(tastes){splitUpTheTasteArrays1.push(tastes.userTaste)})
+  tasteAverage(splitUpTheTasteArrays1)
 }
 
 function tasteAverage(arrayOfTasteArrays) {
-
 
 var tastesX = []
 var tastesY = []

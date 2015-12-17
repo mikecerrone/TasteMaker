eaa5bb61d63ca3a512fd71c658965cd511e110ae
Shawn Watson <shawnw86@gmail.com>
2015-12-16 16:47:38 -0800

    Implement facebook login

--- client/accounts.js
+++ client/accounts.js
@@ -30,3 +30,21 @@ Template.logIn.events({
         Meteor.loginWithPassword(emailVar, passwordVar)
     }
 });
+
+Template.facebook.events({
+    'click #facebook-login': function(event) {
+        Meteor.loginWithFacebook({}, function(err){
+            if (err) {
+                throw new Meteor.Error("Facebook login failed");
+            }
+        });
+    },
+ 
+    'click #logout': function(event) {
+        Meteor.logout(function(err){
+            if (err) {
+                throw new Meteor.Error("Logout failed");
+            }
+        })
+    }
+});

a9208bef8ef6b3f711639e05c24581f1fd9dcdd0
PaulEtscheit <etscheitp@gmail.com>
2015-12-16 19:24:32 -0800

    debugging commit, scanner broken..... again

--- client/recommend_me.js
+++ client/recommend_me.js
@@ -3,7 +3,10 @@ Template.body.events ({
         event.preventDefault();
         $('#pageHome').addClass('hide');
         $('#pageDisplay').removeClass('hide')
-        closest = spiltTastes();
+        searchTerm = Meteor.call('methodsController', function(err, res){
+          return res
+        })
+        console.log(searchTerm)
         render = Blaze.render(Template.recWine, document.querySelector('#pageDisplay'))
     }
 });
@@ -17,116 +20,3 @@ Template.recWine.events ({
     }
 })
 
-var varietalsObject = {
-   'Cabernet Franc':[30,30],
-   'Syrah/Shiraz':[50,18],
-   'Pinot Noir':[-7,10],
-   'Cabernet Sauvignon':[40,5],
-   'Sangiovese':[28,35],
-   'Barbera':[25,-38],
-   'Grenache':[12,-41],
-   'Malbec':[36,-31],
-   'Merlot':[45,-16],
-   "Nero d'Avola":[30,-32],
-   'Petite Sirah':[15,3],
-   'Pinotage':[20,33],
-   'Zinfandel':[35,-35],
-   'Chianti':[15,40],
-   'Tempranillo':[28,30],
-   'Primitivo':[-35,35],
-   'Other Red Wine':[-4,36],
-   'Mourvedre':[10,10],
-   'Nebbiolo':[40,30],
-   'Carmenere':[10,1],
-   'Dolcetto':[-35,25],
-   'Gamay':[-36,-45],
-   'Rhône Blends':[-34,17],
-   'Other Red Blends':[-4,36],
-   'Bordeaux Blends':[-15,40],
-
- //Whites
-   'Chardonnay':[-13,-9],
-   'Sauvignon Blanc':[-27,37],
-   'Pinot Gris/Grigio':[-31,0],
-   'Muscat':[7,45],
-   'Riesling':[-25,-20],
-   'Viognier':[17,-10],
-   'Albarino':[-10, -31],
-   'Chenin Blanc':[-15, 10],
-   'Gewurztraminer':[-20, -25],
-   'Gruner Veltliner':[-20, -25],
-   'Other White Blends':[13, -18],
-   'Other White Wine':[13, -18],
-   'Pinot Blanc':[-20, -13],
-   'Semillon':[-30, 27],
-   'Torrontes':[-45, 7],
-   'Bordeaux White Blends':[42, -21],
-   'Rhône White Blends':[-18, 17]
-  }
-
-function getRecommendation(tasteProfile){
-    // distance cant get bigger then 145 by construct...
-    var smallestDistance = 150;
-    var closest = null;
-    for (var varietalCoordinate in varietalsObject) {
-      var xs = 0;
-      var ys = 0;
-      xs = varietalsObject[varietalCoordinate][0] - tasteProfile[0];
-      xs = xs * xs;
-
-      ys = varietalsObject[varietalCoordinate][1] - tasteProfile[1];
-      ys = ys * ys;
-      var distance = Math.sqrt( xs + ys );
-        if (distance < smallestDistance){
-            smallestDistance = distance;
-            closest = varietalCoordinate;
-        }
-    }
-    console.log('here')
-    console.log(closest)
-    Meteor.call('wineApiRecommendation', closest, function(err, res){
-      console.log('gotresult')
-      console.log(res);
-      wineApiLookupSorting(res, 'recommendation', function(err, res){
-        console.log(res);
-        console.log(err);
-      })
-    });
-}
-
-
-spiltTastes = function() {
-  var splitUpTheTasteArrays1 = []
-  var tastes = Taste.find({})
-  tastes.forEach(function(tastes){splitUpTheTasteArrays1.push(tastes.userTaste)})
-  tasteAverage(splitUpTheTasteArrays1)
-}
-
-function tasteAverage(arrayOfTasteArrays) {
-
-var tastesX = []
-var tastesY = []
-
-var step;
-  for (step = 0; step < arrayOfTasteArrays.length; step++) {
-   tastesX.push(arrayOfTasteArrays[step][0])
-   }
-   var myAvgX = 0;
-   for(var i = 0, len = tastesX.length; i < len; i++) {
-   myAvgX += tastesX[i];
-}
-  tastesX = myAvgX / tastesX.length
-
-
-var step1;
-  for (step1 = 0; step1 < arrayOfTasteArrays.length; step1++) {
-   tastesY.push(arrayOfTasteArrays[step1][1])
-   }
-   var myAvgY = 0;
-   for(var i = 0, len = tastesY.length; i < len; i++) {
-   myAvgY += tastesY[i];
-}
-  tastesY = myAvgY / tastesY.length
-
-  getRecommendation([tastesX, tastesY]);
-}
--- client/wine_searching.js
+++ client/wine_searching.js
@@ -25,7 +25,6 @@ Meteor.startup(function() {
               var sorted = wineApiLookupSorting(res, searchText)
               var results = [sorted, searchText]
               narrowDownSearch(results);
-              // herpewwjfpasca
             });
           },
           function (error) {
@@ -230,76 +229,3 @@ function questionGenerator(flavorElement, callback) {
   // callback();
     return selection;
 };
-
-
-
-// Driver test code for questionServer
- // var bobo = [30,-30]
- // console.log(questionServer(bobo))
-
-
-// Driver test code
- // userEvaluation(wineTasteCoordinates("Cabernet_Franc", "Big_&_Bold"),50,4,-5)
-
-
-
-  wineApiLookupSorting = function(results, wineName) {
-    var wines = {}
-    var wineNoYears = {}
-    var parsedResponse = JSON.parse(results.content);
-    var responseArray = parsedResponse.Products.List
-    for (i = 0; i < responseArray.length; i++) {
-      var nameSplit = responseArray[i].Name.split(" ");
-      var year = nameSplit.pop();
-      var name = nameSplit.join(" ")
-      var region = responseArray[i].Appellation.Region.Name
-      if (responseArray[i].ProductAttributes.length > 0) {
-        var style = responseArray[i].ProductAttributes[0].Name
-      }
-      var varietal = responseArray[i].Varietal.Name
-      var type = responseArray[i].Varietal.WineType.Name
-      var price = responseArray[i].PriceMax
-      if(responseArray[i].Ratings.HighestScore > 0) {
-        var rating = responseArray[i].Ratings.HighestScore
-      } else {
-        var rating = null
-      }
-      wines[name] = {name: name, year: year, price: price, style: style, region: region, varietal: varietal, type: type, rating: rating}
-    }
-    console.log(wines)
-    return similar(wineName, wines)
-    // Meteor.call('similar', wineName, wines);
-  }
-
-  var similar = function(searchWord, resultObject){
-    if (searchWord === "recommendation") {
-      return resultObject
-    } else {
-      var results = []
-      for (var name in resultObject){
-        var lengthsearchWord = searchWord.length;
-        var lengthResult = name.length - 5;
-        var equivalency = 0;
-        var minLength = (lengthsearchWord > lengthResult) ? lengthResult : lengthsearchWord;
-        var maxLength = (lengthsearchWord < lengthResult) ? lengthResult : lengthsearchWord;
-        for(var i = 0; i < minLength; i++) {
-            if(lengthsearchWord[i] == lengthResult[i]) {
-                equivalency++;
-            }
-        }
-
-        var weight = equivalency / maxLength;
-        var alikeness = weight * 100
-        results.push({name: name, score: alikeness})
-
-
-      }
-      var sortedNames = _.sortBy(results, 'score').reverse()
-      var sortedWineObjects = []
-      for(i = 0; i < sortedNames.length; i++){
-        sortedWineObjects.push(resultObject[sortedNames[i].name])
-      }
-
-      return sortedWineObjects
-    }
-  }
